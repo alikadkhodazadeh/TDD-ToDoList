d0f423d260f675a9fb090b1d643978e9cfbce9ba
ali.kdz80 <ali.kdz80@gmail.com>
2022-03-07 18:08:35 +0330

    ToDoList.Domain.Tests.Unit -> Note Constructor Should Throw Exception When Title And Description Is Not Provided

--- ToDoList.Domain.Tests.Unit/NoteTests.cs
+++ ToDoList.Domain.Tests.Unit/NoteTests.cs
@@ -21,5 +21,21 @@ namespace ToDoList.Domain.Tests.Unit
             note.Title.Should().Be(title);
             note.Description.Should().Be(description);
         }
+
+        [Fact]
+        public void Constructor_ShouldThrowException_When_TitleAndDescriptionIsNotProvided()
+        {
+            // Arrange
+            const string? title = "";
+            const string? description = "Test-1";
+
+            // Act
+            /// I wrote as a function not to throw an InvalidOperationException
+            Action note = () => new Note(title, description); 
+
+            // Assert
+            note.Should().Throw<Exception>();
+            note.Should().ThrowExactly<InvalidOperationException>();
+        }
     }
 }
