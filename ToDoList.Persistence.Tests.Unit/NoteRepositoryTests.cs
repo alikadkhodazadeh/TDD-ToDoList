b7595117bfcbff0a1cd36225dae24f062d963e84
ali.kdz80 <ali.kdz80@gmail.com>
2022-03-10 16:19:00 +0330

    Create ToDoList.Persistence.Tests.Unit

---
+++ ToDoList.Persistence.Tests.Unit/NoteRepositoryTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ToDoList.Persistence.Tests.Unit
+{
+    public class NoteRepositoryTests
+    {
+        [Fact]
+        public void Should_Add_New_Note()
+        {
+            // Arrange
+        }
+    }
+}
