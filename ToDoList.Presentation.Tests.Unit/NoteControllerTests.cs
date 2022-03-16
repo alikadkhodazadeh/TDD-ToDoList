c3acca98e36b61a93c25f21e5d9d41bc7328c618
ali.kdz80 <ali.kdz80@gmail.com>
2022-03-17 00:01:54 +0330

    ToDoList.Presentation.Tests.Unit -> NoteControllerTests Should Return List Of All Notes

--- ToDoList.Presentation.Tests.Unit/NoteControllerTests.cs
+++ ToDoList.Presentation.Tests.Unit/NoteControllerTests.cs
@@ -1,11 +1,21 @@
 ﻿using NSubstitute;
+using System.Collections.Generic;
 using ToDoList.Application;
+using ToDoList.Domain;
 using ToDoList.Presentation.Controllers;
 
 namespace ToDoList.Presentation.Tests.Unit;
 
 public class NoteControllerTests
 {
+    private readonly NotesController _notesController;
+    private readonly INoteRepository _noteRepository;
+    public NoteControllerTests()
+    {
+        _noteRepository = Substitute.For<INoteRepository>();
+        _notesController = new NotesController(_noteRepository);
+    }
+
     [Fact]
     public void Should_Return_All_Notes()
     {
@@ -18,5 +28,18 @@ public class NoteControllerTests
 
         // Assert
         repository.Received().GetAll();
+    }
+
+    [Fact]
+    public void Should_Return_List_Of_All_Notes()
+    {
+        // Arrange
+        _noteRepository.GetAll().Returns(new List<Note>());
+
+        // Act
+        var notes = _notesController.Get();
+
+        // Assert
+        notes.Should().BeOfType<List<Note>>();
     }
 }
